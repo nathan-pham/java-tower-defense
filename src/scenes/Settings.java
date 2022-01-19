f838acf9391a87c817ec712298375d27078066b2
Nathan Pham <nathanpham.me@gmail.com>
2022-01-19 10:51:49 -0800

    add settings button

--- src/scenes/Settings.java
+++ src/scenes/Settings.java
@@ -1,13 +1,20 @@
 package src.scenes;
 
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Color;
+
+import static src.main.GameStates.*;
 import src.main.Game;
+
+import src.ui.Button;
 
 public class Settings extends GameScene implements SceneMethods {
 
+    Button bMenu;
+
     public Settings(Game game) {
         super(game);
+        initButtons();
     }
 
     @Override
@@ -15,31 +22,24 @@ public class Settings extends GameScene implements SceneMethods {
 
         ctx.setColor(Color.BLACK);
         ctx.fillRect(0, 0, game.SIZE, game.SIZE);
+        bMenu.draw(ctx);
 
     }
 
+    // initialize buttons
+    public void initButtons() {
+        buttons.add(bMenu = new Button("Menu", 10, 10, 100, 45));
+    }
+
+    // event listeners
     @Override
     public void mouseClicked(int x, int y) {
-        // TODO Auto-generated method stub
-        
-    }
 
-    @Override
-    public void mouseMoved(int x, int y) {
-        // TODO Auto-generated method stub
+        setCursorState(DEFAULT_MOUSE);
+        if (bMenu.getBounds().contains(x, y)) {
+            SetGameState(MENU);
+        }
 
-    }
-
-    @Override
-    public void mousePressed(int x, int y) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void mouseReleased(int x, int y) {
-        // TODO Auto-generated method stub
-        
     }
 
 }
